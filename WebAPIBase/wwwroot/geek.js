954f208611c539a6808d0d9c1d1abadfe15159d5
Körhöcz Bence <korhoczbence@gmail.com>
2023-11-29 15:49:22 +0100

    Add project files.

---
+++ WebAPIBase/wwwroot/geek.js
@@ -0,0 +1,54 @@
+﻿window.onload = function() { 
+    letoltes();
+}
+
+var viccek;
+function letoltes() {
+    fetch("jokes.json")
+        .then(response => response.json())
+        .then(data => letöltésBefejeződött(data)
+        );
+}
+
+function letöltésBefejeződött(d) {
+    console.log("Sikeres letöltés")
+    console.log(d)
+    viccek = d;
+
+    //for ciklusos rész
+    for (var i = 0; i < 20; i++) {
+        var ujDiv = document.createElement("div");
+        ujDiv.classList.add("jokeDiv");
+        document.getElementById("jokeshaha").appendChild(Divsor);
+
+        var viccecske = new Object();
+        viccecske.text = viccek.text;
+        viccecske.question = viccek.question;
+        viccecske.answer = viccek.answer;
+        viccecske.author = viccek.author;
+        viccecske.created = viccek.created;
+        viccecske.tags = viccek.tags;
+        viccecske.rating = viccek.rating;
+
+        //DIV feltöltése anyaggal:
+        var cim = document.createElement("h3");
+        cim.classList.add("cim");
+        cim.appendChild(ujDiv);
+        cim.innerHTML = i.toString() + "kérdés";
+
+        var kerdes = document.createElement("p");
+        kerdes.classList.add("kerdes");
+        kerdes.appendChild(ujDiv);
+        kerdes.innerHTML = viccecske.question;
+
+        var valasz = document.createElement("p");
+        valasz.classList.add("valasz");
+        valasz.appendChild(ujDiv);
+        valasz.innerHTML = viccecske.answer;
+
+        var szerzo = document.createElement("p");
+        szerzo.classList.add("szerzo");
+        szerzo.appendChild(ujDiv);
+        szerzo.innerHTML = viccecske.author;
+    }
+}
---
+++ WebAPIBase/wwwroot/scripts/pascal.js
@@ -0,0 +1,49 @@
+﻿window.onload = function () {
+    initPascal(13); //célszerű, hogy csak akkor fussanak le a függvények,
+    //ha betöltött az oldal, különben hiba esetén kihal az egész, mint a dínók
+};
+
+var initPascal = function (n) {
+    for (var sor = 0; sor < n; sor++) {
+        var Divsor = document.createElement("div");
+        //új div létrehozása az új sornak
+
+        Divsor.classList.add("sor");
+        //új div osztálylistájához add hozzá a "sor"-t
+
+        document.getElementById("pascal").appendChild(Divsor);
+        //új div-et add hozzá a "pascal" gyermekeihez
+
+        for (var oszlop = 0; oszlop <= sor; oszlop++) {
+            var DivElem = document.createElement("div");
+            //új div létrehozása az új elemnek
+
+            DivElem.classList.add("elem");
+            //új elem div osztálylistájához add hozzá az "elem"-et
+
+            //DivElem.innerHTML = `${sor}:${oszlop}`;
+            //teszteléshet .innerHTML = `${sor}:${oszlop}`
+
+            DivElem.innerHTML = pascalSorCsicska(sor,oszlop);
+            //legyen az innerHTML a megfelelő szám
+
+            Divsor.appendChild(DivElem);
+            //új elem div-et vedd fel a sor elemei közé
+        }
+    }
+}
+
+
+var faktorialis = function (n) {
+    let er = 1;
+    for (let i = 2; i <= n; i++) {
+        er = er * i;
+    }
+    return er;
+}
+
+var pascalSorCsicska = function (n,k) {
+    let sanyi = faktorialis(n) / (faktorialis(k) * faktorialis(n - k));
+    return sanyi;
+}
+
